1dcf74a49128f762acd1671c26218e4d7b5a457d
Sascha Schwabbauer <sascha.schwabbauer@9elements.com>
2015-03-16 16:35:34 +0100

    Added an Objective-C sample project

---
+++ Samples/imglyExampleObjC/imglyExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  imglyExample
+//
+//  Created by Sascha Schwabbauer on 16/03/15.
+//  Copyright (c) 2015 9elements GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Samples/imglyExampleObjC/imglyExample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  imglyExample
+//
+//  Created by Sascha Schwabbauer on 16/03/15.
+//  Copyright (c) 2015 9elements GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
