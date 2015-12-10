b6027bce3ef3e2cba97c54b5702e5ca931cfad1e
Carsten Przyluczky <carsten.przyluczky@9elements.com>
2015-12-10 14:52:36 +0100

    removed crop view from orientation dialog

--- imglyKit/Frontend/Editor/IMGLYOrientationEditorViewController.swift
+++ imglyKit/Frontend/Editor/IMGLYOrientationEditorViewController.swift
@@ -52,17 +52,6 @@ public class IMGLYOrientationEditorViewController: IMGLYSubEditorViewController 
         return button
         }()
     
-    private lazy var transparentRectView: UIView = {
-        let view = UIView()
-        view.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.8)
-        return view
-        }()
-    
-    private let cropRectComponent = IMGLYInstanceFactory.cropRectComponent()
-    private var cropRectLeftBound = CGFloat(0)
-    private var cropRectRightBound = CGFloat(0)
-    private var cropRectTopBound = CGFloat(0)
-    private var cropRectBottomBound = CGFloat(0)
     
     // MARK: - UIViewController
     
@@ -73,7 +62,6 @@ public class IMGLYOrientationEditorViewController: IMGLYSubEditorViewController 
         navigationItem.title = NSLocalizedString("orientation-editor.title", tableName: nil, bundle: bundle, value: "", comment: "")
         
         configureButtons()
-        configureCropRect()
     }
     
     public override func viewDidAppear(animated: Bool) {
@@ -84,19 +72,12 @@ public class IMGLYOrientationEditorViewController: IMGLYSubEditorViewController 
             cropRect.size.width != 1.0 || cropRect.size.height != 1.0 {
                 updatePreviewImageWithoutCropWithCompletion {
                     self.view.layoutIfNeeded()
-                    self.cropRectComponent.present()
-                    self.layoutCropRectViews()
                 }
-        } else {
-            layoutCropRectViews()
         }
     }
     
     public override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        
-        transparentRectView.frame = view.convertRect(previewImageView.visibleImageFrame, fromView: previewImageView)
-        reCalculateCropRectBounds()
     }
     
     // MARK: - IMGLYEditorViewController
@@ -146,129 +127,44 @@ public class IMGLYOrientationEditorViewController: IMGLYSubEditorViewController 
         buttonContainerView.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|[flipVerticallyButton]|", options: [], metrics: nil, views: views))
         
         // Container Constraints
-        
         bottomContainerView.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|[buttonContainerView]|", options: [], metrics: nil, views: views))
         bottomContainerView.addConstraint(NSLayoutConstraint(item: buttonContainerView, attribute: .CenterX, relatedBy: .Equal, toItem: bottomContainerView, attribute: .CenterX, multiplier: 1, constant: 0))
     }
     
-    private func configureCropRect() {
-        view.addSubview(transparentRectView)
-        cropRectComponent.cropRect = fixedFilterStack.orientationCropFilter.cropRect
-        cropRectComponent.setup(transparentRectView, parentView: self.view, showAnchors: false)
-    }
     
     // MARK: - Helpers
     
     private func updatePreviewImageWithoutCropWithCompletion(completionHandler: IMGLYPreviewImageGenerationCompletionBlock?) {
-        let oldCropRect = fixedFilterStack.orientationCropFilter.cropRect
-        fixedFilterStack.orientationCropFilter.cropRect = CGRect(x: 0, y: 0, width: 1, height: 1)
         updatePreviewImageWithCompletion { () -> (Void) in
-            self.fixedFilterStack.orientationCropFilter.cropRect = oldCropRect
             completionHandler?()
         }
     }
     
-    // MARK: - Cropping
-    
-    private func layoutCropRectViews() {
-        reCalculateCropRectBounds()
-        let viewWidth = cropRectRightBound - cropRectLeftBound
-        let viewHeight = cropRectBottomBound - cropRectTopBound
-        let x = cropRectLeftBound + viewWidth * fixedFilterStack.orientationCropFilter.cropRect.origin.x
-        let y = cropRectTopBound + viewHeight * fixedFilterStack.orientationCropFilter.cropRect.origin.y
-        let width = viewWidth * fixedFilterStack.orientationCropFilter.cropRect.size.width
-        let height = viewHeight * fixedFilterStack.orientationCropFilter.cropRect.size.height
-        let rect = CGRect(x: x, y: y, width: width, height: height)
-        cropRectComponent.cropRect = rect
-        cropRectComponent.layoutViewsForCropRect()
-    }
-    
-    private func reCalculateCropRectBounds() {
-        let width = transparentRectView.frame.size.width
-        let height = transparentRectView.frame.size.height
-        cropRectLeftBound = (width - previewImageView.visibleImageFrame.size.width) / 2.0
-        cropRectRightBound = width - cropRectLeftBound
-        cropRectTopBound = (height - previewImageView.visibleImageFrame.size.height) / 2.0
-        cropRectBottomBound = height - cropRectTopBound
-    }
-    
-    private func rotateCropRectLeft() {
-        moveCropRectMidToOrigin()
-        // rotatate
-        let tempRect = fixedFilterStack.orientationCropFilter.cropRect
-        fixedFilterStack.orientationCropFilter.cropRect.origin.x = tempRect.origin.y
-        fixedFilterStack.orientationCropFilter.cropRect.origin.y = -tempRect.origin.x
-        fixedFilterStack.orientationCropFilter.cropRect.size.width = tempRect.size.height
-        fixedFilterStack.orientationCropFilter.cropRect.size.height = -tempRect.size.width
-        moveCropRectTopLeftToOrigin()
-    }
-    
-    private func rotateCropRectRight() {
-        moveCropRectMidToOrigin()
-        // rotatate
-        let tempRect = fixedFilterStack.orientationCropFilter.cropRect
-        fixedFilterStack.orientationCropFilter.cropRect.origin.x = -tempRect.origin.y
-        fixedFilterStack.orientationCropFilter.cropRect.origin.y = tempRect.origin.x
-        fixedFilterStack.orientationCropFilter.cropRect.size.width = -tempRect.size.height
-        fixedFilterStack.orientationCropFilter.cropRect.size.height = tempRect.size.width
-        moveCropRectTopLeftToOrigin()
-    }
-    
-    private func flipCropRectHorizontal() {
-        moveCropRectMidToOrigin()
-        fixedFilterStack.orientationCropFilter.cropRect.origin.x = -fixedFilterStack.orientationCropFilter.cropRect.origin.x - fixedFilterStack.orientationCropFilter.cropRect.size.width
-        moveCropRectTopLeftToOrigin()
-    }
-    
-    private func flipCropRectVertical() {
-        moveCropRectMidToOrigin()
-        fixedFilterStack.orientationCropFilter.cropRect.origin.y = -fixedFilterStack.orientationCropFilter.cropRect.origin.y - fixedFilterStack.orientationCropFilter.cropRect.size.height
-        moveCropRectTopLeftToOrigin()
-    }
-    
-    private func moveCropRectMidToOrigin() {
-        fixedFilterStack.orientationCropFilter.cropRect.origin.x -= 0.5
-        fixedFilterStack.orientationCropFilter.cropRect.origin.y -= 0.5
-    }
-    
-    private func moveCropRectTopLeftToOrigin() {
-        fixedFilterStack.orientationCropFilter.cropRect.origin.x += 0.5
-        fixedFilterStack.orientationCropFilter.cropRect.origin.y += 0.5
-    }
-
     // MARK: - Actions
     
     @objc private func rotateLeft(sender: IMGLYImageCaptionButton) {
         fixedFilterStack.orientationCropFilter.rotateLeft()
-        rotateCropRectLeft()
         updatePreviewImageWithoutCropWithCompletion {
             self.view.layoutIfNeeded()
-            self.layoutCropRectViews()
         }
     }
     
     @objc private func rotateRight(sender: IMGLYImageCaptionButton) {
         fixedFilterStack.orientationCropFilter.rotateRight()
-        rotateCropRectRight()
         updatePreviewImageWithoutCropWithCompletion {
             self.view.layoutIfNeeded()
-            self.layoutCropRectViews()
         }
     }
     
     @objc private func flipHorizontally(sender: IMGLYImageCaptionButton) {
         fixedFilterStack.orientationCropFilter.flipHorizontal()
-        flipCropRectHorizontal()
         updatePreviewImageWithoutCropWithCompletion {
-            self.layoutCropRectViews()
         }
     }
     
     @objc private func flipVertically(sender: IMGLYImageCaptionButton) {
         fixedFilterStack.orientationCropFilter.flipVertical()
-        flipCropRectVertical()
         updatePreviewImageWithoutCropWithCompletion {
-            self.layoutCropRectViews()
         }
     }
 }
