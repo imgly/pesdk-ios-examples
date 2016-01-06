168e2c3948b4e415645873538ee7c237c875b9a9
Sascha Schwabbauer <sascha.schwabbauer@9elements.com>
2016-01-06 16:20:01 +0100

    Add fastlane and update travis

---
+++ fastlane/actions/swiftlint.rb
@@ -0,0 +1,58 @@
+module Fastlane
+  module Actions
+    class SwiftlintAction < Action
+      def self.run(params)
+        if `which swiftlint`.to_s.length == 0 and !Helper.test?
+          raise "You have to install swiftlint using `brew install swiftlint`".red
+        end
+
+        command = 'swiftlint lint'
+        command << " --strict" if params[:strict]
+        command << " --config #{params[:config_file]}" if params[:config_file]
+        command << " > #{params[:output_file]}" if params[:output_file]
+        Actions.sh(command)
+      end
+
+      #####################################################
+      # @!group Documentation
+      #####################################################
+
+      def self.description
+        "Run swift code validation using SwiftLint"
+      end
+
+      def self.details
+      end
+
+      def self.available_options
+        [
+          FastlaneCore::ConfigItem.new(key: :output_file,
+                                       description: 'Path to output SwiftLint result',
+                                       optional: true),
+          FastlaneCore::ConfigItem.new(key: :config_file,
+                                       description: 'Custom configuration file of SwiftLint',
+                                       optional: true),
+          FastlaneCore::ConfigItem.new(key: :strict,
+                                       description: 'Fail on warnings? (true/false)',
+                                       default_value: false,
+                                       is_string: false,
+                                       optional: true)
+        ]
+      end
+
+      def self.output
+      end
+
+      def self.return_value
+      end
+
+      def self.authors
+        ["KrauseFx"]
+      end
+
+      def self.is_supported?(platform)
+        [:ios, :mac].include?(platform)
+      end
+    end
+  end
+end
