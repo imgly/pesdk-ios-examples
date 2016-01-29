3927625dfd06c01feb1fb0d83b180ce9543be7b3
Carsten Przyluczky <carsten.przyluczky@9elements.com>
2016-01-29 13:59:37 +0100

    fixed autolayout issues of the color picker

--- imglyKit/Frontend/Editor/ColorPickerView.swift
+++ imglyKit/Frontend/Editor/ColorPickerView.swift
@@ -92,17 +92,12 @@ import UIKit
 
         NSLayoutConstraint(item: saturationBrightnessPickerView, attribute: .Height, relatedBy: .Equal, toItem: saturationBrightnessPickerView, attribute: .Width, multiplier: 1, constant: 0).active = true
 
+        self.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|-10-[alphaPickerView(==20)]-10-[saturationBrightnessPickerView]-10-[huePickerView(==20)]-10-[colorView(>=20)]-(110@750)-|", options: [], metrics: nil, views: views))
+
         self.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("|-20-[saturationBrightnessPickerView]-20-|", options: [], metrics: nil, views: views))
-        self.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|-10-[alphaPickerView]-10-[saturationBrightnessPickerView]-10-[huePickerView]-10-[colorView]", options: [], metrics: nil, views: views))
-
-        self.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("|-120-[colorView]-120-|", options: [], metrics: nil, views: views))
-        colorView.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:[colorView(==40)]", options: [], metrics: nil, views: views))
-
+        self.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("|-20-[colorView]-20-|", options: [], metrics: nil, views: views))
         self.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("|-20-[huePickerView]-20-|", options: [], metrics: nil, views: views))
-        huePickerView.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:[huePickerView(==20)]", options: [], metrics: nil, views: views))
-
         self.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("|-20-[alphaPickerView]-20-|", options: [], metrics: nil, views: views))
-        alphaPickerView.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:[alphaPickerView(==20)]", options: [], metrics: nil, views: views))
     }
 }
 
