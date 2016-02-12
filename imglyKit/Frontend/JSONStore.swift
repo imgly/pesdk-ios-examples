c437a6dfa9395aae2be59ecf57a8ac4a66008cf4
Carsten Przyluczky <carsten.przyluczky@9elements.com>
2016-02-12 22:58:36 +0100

    JSON store

--- imglyKit/Frontend/JSONStore.swift
+++ imglyKit/Frontend/JSONStore.swift
@@ -9,5 +9,37 @@
 import UIKit
 
 class JSONStore: NSObject {
+    static private var store: String? = nil
 
+    static func httpGet(request: NSURLRequest!, callback: (String, String?) -> Void) {
+        let session = NSURLSession.sharedSession()
+        let task = session.dataTaskWithRequest(request) {
+            (data, response, error) -> Void in
+            if let error = error {
+                callback("", error.localizedDescription)
+            } else {
+                let result = NSString(data: data!, encoding:
+                    NSASCIIStringEncoding)!
+                store = result as String
+                callback(result as String, nil)
+            }
+        }
+        task.resume()
+    }
+
+    static func doIt() {
+        let request = NSMutableURLRequest(URL: NSURL(string: "http://localhost:8000/borders.json")!)
+        if let store = store {
+            print("got it", store)
+        } else {
+            httpGet(request) {
+                (data, error) -> Void in
+                if error != nil {
+                    print(error)
+                } else {
+                    print(data)
+                }
+            }
+        }
+    }
 }
