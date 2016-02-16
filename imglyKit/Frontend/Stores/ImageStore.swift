44b0cd8fc69124cf11bde222a9cd7eb8bd2998d4
Carsten Przyluczky <carsten.przyluczky@9elements.com>
2016-02-16 12:49:12 +0100

    finished image store

--- imglyKit/Frontend/Stores/ImageStore.swift
+++ imglyKit/Frontend/Stores/ImageStore.swift
@@ -53,28 +53,27 @@ import UIKit
         if let dict = store[url] {
             completionBlock(dict, nil)
         } else {
- //           startRequest(url, completionBlock: completionBlock)
+            startRequest(url, completionBlock: completionBlock)
         }
     }
 
-  /*  private func startJSONRequest(url: String, completionBlock: (NSDictionary?, NSError?) -> Void) {
+    private func startRequest(url: String, completionBlock: (UIImage?, NSError?) -> Void) {
         let request = NSMutableURLRequest(URL: NSURL(string: url)!)
         httpGet(request) {
             (data, error) -> Void in
             if error != nil {
                 completionBlock(nil, error)
             } else {
-                print("1", data)
                 if let data = data {
-                    print("3")
-                    if let dict = self.dictionaryFromData(data) {
-                        print("2", dict)
-                        self.store[url] = dict
-                        completionBlock(dict, nil)
+                    if let image = UIImage(data: data) {
+                        self.store[url] = image
+                        completionBlock(image, nil)
+                    } else {
+                        completionBlock(nil, NSError(info: "No image found at \(url)."))
                     }
                 }
             }
         }
 
-    }*/
+    }
 }
