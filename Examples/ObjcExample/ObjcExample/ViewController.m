b1e392c17222a84e5a02fe2add889cf52892e802
Sascha Schwabbauer <sascha.schwabbauer@9elements.com>
2017-09-22 17:29:16 +0200

    Update Objective-C code for v8

--- Examples/ObjcExample/ObjcExample/ViewController.m
+++ Examples/ObjcExample/ObjcExample/ViewController.m
@@ -19,89 +19,84 @@
 #pragma mark - UITableViewDelegate
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (indexPath.row == 0) {
-        [self presentCameraViewController];
-    } else if (indexPath.row == 1) {
-        [self presentPhotoEditViewController];
-    } else if (indexPath.row == 2) {
-        [self pushPhotoEditViewController];
-    }
+  if (indexPath.row == 0) {
+    [self presentCameraViewController];
+  } else if (indexPath.row == 1) {
+    [self presentPhotoEditViewController];
+  } else if (indexPath.row == 2) {
+    [self pushPhotoEditViewController];
+  }
 }
 
 #pragma mark - Configuration
 
 - (void)configureStickers {
-    // Duplicate the first sticker category for demonstration purposes
-
-    PESDKStickerCategory *stickerCategory = PESDKStickerCategory.all.firstObject;
-    PESDKStickerCategory.all = [PESDKStickerCategory.all arrayByAddingObject:stickerCategory];
+  // Duplicate the first sticker category for demonstration purposes
+  
+  PESDKStickerCategory *stickerCategory = PESDKStickerCategory.all.firstObject;
+  PESDKStickerCategory.all = [PESDKStickerCategory.all arrayByAddingObject:stickerCategory];
 }
 
 - (PESDKConfiguration *)buildConfiguration {
-    PESDKConfiguration *configuration = [[PESDKConfiguration alloc] initWithBuilder:^(PESDKConfigurationBuilder * _Nonnull builder) {
-        // Configure camera
-        [builder configureCameraViewController:^(PESDKCameraViewControllerOptionsBuilder * _Nonnull options) {
-            // Just enable Photos
-            options.allowedRecordingModesAsNSNumbers = @[@(RecordingModePhoto)];
-        }];
+  PESDKConfiguration *configuration = [[PESDKConfiguration alloc] initWithBuilder:^(PESDKConfigurationBuilder * _Nonnull builder) {
+    // Configure camera
+    [builder configureCameraViewController:^(PESDKCameraViewControllerOptionsBuilder * _Nonnull options) {
+      // Just enable Photos
+      options.allowedRecordingModesAsNSNumbers = @[@(RecordingModePhoto)];
     }];
-
-    return configuration;
+  }];
+  
+  return configuration;
 }
 
 #pragma mark - Presentation
 
 - (void)presentCameraViewController {
-    [self configureStickers];
-    PESDKConfiguration *configuration = [self buildConfiguration];
-    PESDKCameraViewController *cameraViewController = [[PESDKCameraViewController alloc] initWithConfiguration:configuration];
-    __weak PESDKCameraViewController *weakCameraViewController = cameraViewController;
-    cameraViewController.completionBlock = ^(UIImage * _Nullable image, NSURL * _Nullable videoURL) {
-        [weakCameraViewController presentViewController:[self createPhotoEditViewControllerWithPhoto:image] animated:YES completion:nil];
-    };
-
-    [self presentViewController:cameraViewController animated:YES completion:nil];
+  [self configureStickers];
+  PESDKConfiguration *configuration = [self buildConfiguration];
+  PESDKCameraViewController *cameraViewController = [[PESDKCameraViewController alloc] initWithConfiguration:configuration];
+  __weak PESDKCameraViewController *weakCameraViewController = cameraViewController;
+  cameraViewController.completionBlock = ^(UIImage * _Nullable image, NSURL * _Nullable videoURL) {
+    [weakCameraViewController presentViewController:[self createPhotoEditViewControllerWithPhoto:image] animated:YES completion:nil];
+  };
+  
+  [self presentViewController:cameraViewController animated:YES completion:nil];
 }
 
-- (PESDKToolbarController *)createPhotoEditViewControllerWithPhoto:(UIImage *)photo {
-    PESDKConfiguration *configuration = [self buildConfiguration];
-    NSMutableArray<PESDKMenuItem *> *menuItems = [[PESDKMenuItem defaultItemsWithConfiguration:configuration] mutableCopy];
-    [menuItems removeLastObject]; // Remove last meu item ('Magic')
-
-    // Create a photo edit view controller
-    PESDKPhotoEditViewController *photoEditViewController = [[PESDKPhotoEditViewController alloc] initWithPhoto:photo menuItems:menuItems configuration:configuration];
-    photoEditViewController.delegate = self;
-
-    // A PhotoEditViewController works in conjunction with a `ToolbarController`, so in almost
-    // all cases it should be embedded in one and presented together.
-    PESDKToolbarController *toolbarController = [[PESDKToolbarController alloc] init];
-    [toolbarController pushViewController:photoEditViewController animated:NO completion:nil];
-
-    return toolbarController;
+- (PESDKPhotoEditViewController *)createPhotoEditViewControllerWithPhoto:(UIImage *)photo {
+  PESDKConfiguration *configuration = [self buildConfiguration];
+  NSMutableArray<PESDKPhotoEditMenuItem *> *menuItems = [[PESDKPhotoEditMenuItem defaultItems] mutableCopy];
+  [menuItems removeLastObject]; // Remove last menu item ('Magic')
+  
+  // Create a photo edit view controller
+  PESDKPhotoEditViewController *photoEditViewController = [[PESDKPhotoEditViewController alloc] initWithPhoto:photo configuration:configuration menuItems:menuItems photoEditModel:[[PESDKPhotoEditModel alloc] init]];
+  photoEditViewController.delegate = self;
+  
+  return photoEditViewController;
 }
 
 - (void)presentPhotoEditViewController {
-    UIImage *photo = [UIImage imageNamed:@"LA.jpg"];
-    [self presentViewController:[self createPhotoEditViewControllerWithPhoto:photo] animated:YES completion:nil];
+  UIImage *photo = [UIImage imageNamed:@"LA.jpg"];
+  [self presentViewController:[self createPhotoEditViewControllerWithPhoto:photo] animated:YES completion:nil];
 }
 
 - (void)pushPhotoEditViewController {
-    UIImage *photo = [UIImage imageNamed:@"LA.jpg"];
-    [self.navigationController pushViewController:[self createPhotoEditViewControllerWithPhoto:photo] animated:YES];
+  UIImage *photo = [UIImage imageNamed:@"LA.jpg"];
+  [self.navigationController pushViewController:[self createPhotoEditViewControllerWithPhoto:photo] animated:YES];
 }
 
 #pragma mark - PhotoEditViewControllerDelegate
 
 - (void)photoEditViewController:(PESDKPhotoEditViewController *)photoEditViewController didSaveImage:(UIImage *)image imageAsData:(NSData *)data {
-    [self dismissViewControllerAnimated:YES completion:nil];
+  [self dismissViewControllerAnimated:YES completion:nil];
 }
 
 - (void)photoEditViewControllerDidFailToGeneratePhoto:(PESDKPhotoEditViewController *)photoEditViewController {
-    [self dismissViewControllerAnimated:YES completion:nil];
+  [self dismissViewControllerAnimated:YES completion:nil];
 }
 
 - (void)photoEditViewControllerDidCancel:(PESDKPhotoEditViewController *)photoEditviewController {
-    [self dismissViewControllerAnimated:YES completion:nil];
+  [self dismissViewControllerAnimated:YES completion:nil];
 }
 
 @end
